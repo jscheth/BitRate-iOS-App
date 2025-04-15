df796f01d714e84ac6edb0e35eb8404668ca2138
jscheth <sovichea.chet@gmail.com>
2025-04-14 17:37:55 -0700

    Initial Commit

---
+++ BitRateTests/BitRateTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitRateTests.swift
+//  BitRateTests
+//
+//  Created by Jonathan Cheth on 4/14/25.
+//
+
+import Testing
+@testable import BitRate
+
+struct BitRateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

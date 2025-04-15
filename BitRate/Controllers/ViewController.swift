f2b6a65c2aa7e3a959e26fb86dea1428ccd5f237
jscheth <sovichea.chet@gmail.com>
2025-04-15 14:55:27 -0700

    Add fetching the coin price request

--- BitRate/Controllers/ViewController.swift
+++ BitRate/Controllers/ViewController.swift
@@ -7,17 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPickerView: UIPickerView!
     
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPickerView.dataSource = self
+        currencyPickerView.delegate = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
     }
 
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
+        let currency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: currency)
+    }
 
 }
 

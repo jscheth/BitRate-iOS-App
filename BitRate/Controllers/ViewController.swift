e9284453ba9facff4e02eb2ed3c01fd752389940
jscheth <sovichea.chet@gmail.com>
2025-04-15 14:14:09 -0700

    Add the assets and color sets

--- BitRate/Controllers/ViewController.swift
+++ BitRate/Controllers/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ BitRate/Models/CoinManager.swift
@@ -0,0 +1,16 @@
+//
+//  CoinManager.swift
+//  BitRate
+//
+//  Created by Jonathan Cheth on 4/15/25.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
+    
+}
